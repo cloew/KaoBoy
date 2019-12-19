c0eea6e1c18f98549dd9429b74a511ad077858b0
Chris Loew <cloew123@gmail.com>
2019-12-18 19:47:12 -0800

    Added zero flag interaction

--- src/registers.rs
+++ src/registers.rs
@@ -1,5 +1,7 @@
 use crate::{build_u16, get_lower_u8, get_upper_u8};
 
+const ZERO_FLAG_MASK: u8 = 0x80;
+
 pub struct Registers {
     pub a: u8,
     pub b: u8,
@@ -19,6 +21,18 @@ impl Registers {
     fn write_bc_register(&mut self, value: u16) {
         self.b = get_upper_u8!(value); //((value & 0xFF00) >> 8) as u8;
         self.c = get_lower_u8!(value);
+    }
+    
+    fn get_zero_flag(&self) -> bool {
+        return self.f & ZERO_FLAG_MASK > 0;
+    }
+    
+    fn set_zero_flag(&mut self, is_zero: bool) {
+        if is_zero {
+            self.f |= ZERO_FLAG_MASK;
+        } else {
+            self.f &= !ZERO_FLAG_MASK;
+        }
     }
 }
 
@@ -48,5 +62,51 @@ mod tests {
         
         assert_eq!(as_hex!(registers.b), as_hex!(EXPECTED_B_VALUE));
         assert_eq!(as_hex!(registers.c), as_hex!(EXPECTED_C_VALUE));
+    }
+    
+    #[test]
+    fn test_get_zero_flag_true_reads_flag_properly() {
+        const INITIAL_FLAGS: u8 = 0x80;
+        
+        let mut registers = Registers {a: 0, b: 0, c: 0, d: 0, e: 0, f: INITIAL_FLAGS, h: 0, l: 0};
+        
+        let zero_flag = registers.get_zero_flag();
+        
+        assert_eq!(zero_flag, true);
+    }
+    
+    #[test]
+    fn test_get_zero_flag_false_reads_flag_properly() {
+        const INITIAL_FLAGS: u8 = 0x70;
+        
+        let mut registers = Registers {a: 0, b: 0, c: 0, d: 0, e: 0, f: INITIAL_FLAGS, h: 0, l: 0};
+        
+        let zero_flag = registers.get_zero_flag();
+        
+        assert_eq!(zero_flag, false);
+    }
+    
+    #[test]
+    fn test_set_zero_flag_to_true_flags_only_the_zero_bit() {
+        const INITIAL_FLAGS: u8 = 0x70;
+        const EXPECTED_FLAGS: u8 = 0xF0;
+        
+        let mut registers = Registers {a: 0, b: 0, c: 0, d: 0, e: 0, f: INITIAL_FLAGS, h: 0, l: 0};
+        
+        registers.set_zero_flag(true);
+        
+        assert_eq!(as_hex!(registers.f), as_hex!(EXPECTED_FLAGS));
+    }
+    
+    #[test]
+    fn test_set_zero_flag_to_false_flags_only_the_zero_bit() {
+        const INITIAL_FLAGS: u8 = 0xF0;
+        const EXPECTED_FLAGS: u8 = 0x70;
+        
+        let mut registers = Registers {a: 0, b: 0, c: 0, d: 0, e: 0, f: INITIAL_FLAGS, h: 0, l: 0};
+        
+        registers.set_zero_flag(false);
+        
+        assert_eq!(as_hex!(registers.f), as_hex!(EXPECTED_FLAGS));
     }
 }
