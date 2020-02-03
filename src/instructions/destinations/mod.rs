f8f1be140e582d8e68b8b7749c83e2f3d3611819
Chris Loew <cloew123@gmail.com>
2020-02-02 19:25:58 -0800

    Added a register destintion

--- src/instructions/destinations/mod.rs
+++ src/instructions/destinations/mod.rs
@@ -1,1 +1,2 @@
 pub mod destination;
+pub mod register_destination;
---
+++ src/instructions/destinations/register_destination.rs
@@ -0,0 +1,37 @@
+use crate::instructions::destinations::destination::Destination;
+use crate::registers::register_names::RegisterName;
+use crate::registers::registers::Registers;
+
+pub struct RegisterDestination {
+    _name: RegisterName,
+}
+
+impl RegisterDestination {
+	pub fn new(register_name: RegisterName) -> RegisterDestination {
+		return RegisterDestination {_name: register_name};
+	}
+}
+
+impl Destination for RegisterDestination {
+	fn assign(&self, registers: &mut Registers, new_value: u8) {
+        registers.get_mut(self._name).set(new_value);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{as_hex};
+    
+    #[test]
+    fn test_run_returns_register_value() {
+        const EXPECTED_A: u8 = 0x12;
+        let mut registers = Registers::new();
+        registers.a.set(0x00);
+        let source = RegisterDestination::new(RegisterName::A);
+        
+        source.assign(&mut registers, EXPECTED_A);
+        
+        assert_eq!(as_hex!(registers.a), as_hex!(EXPECTED_A));
+    }
+}
