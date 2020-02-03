c678efcc0a74371a6e1b5beca39c08a28e6a64b6
Chris Loew <cloew123@gmail.com>
2020-02-02 20:11:37 -0800

    Updated add to use the new Bianry Op Instruction class

--- src/instructions/add/add.rs
+++ src/instructions/add/add.rs
@@ -1,15 +1,15 @@
 use crate::registers::registers::Registers;
 use crate::instructions::utils::half_carry_utils::{check_half_carry};
 
-pub fn add(registers: &mut Registers, value: u8) {
-    let original_value = registers.a.get();
-    let (new_value, overflow) = registers.a.overflowing_add(value);
+pub fn add(registers: &mut Registers, left_value: u8, right_value: u8) -> u8 {
+    let (new_value, overflow) = left_value.overflowing_add(right_value);
     
-    registers.a.set(new_value);
     registers.zero_flag.set(new_value == 0);
     registers.subtract_flag.reset();
     registers.carry_flag.set(overflow);
-    registers.half_carry_flag.set(check_half_carry(original_value, value));
+    registers.half_carry_flag.set(check_half_carry(left_value, right_value));
+    
+    return new_value;
 }
 
 #[cfg(test)]
@@ -18,111 +18,100 @@ mod tests {
     use crate::{as_hex};
     
     #[test]
-    fn test_add_adds_to_a_register() {
-        const INITIAL_A: u8 = 0x12;
-        const VALUE_TO_ADD: u8 = 0x34;
-        const EXPECTED_VALUE: u16 = 0x46;
-        
+    fn test_add_returns_added_value() {
+        const LEFT_VALUE: u8 = 0x12;
+        const RIGHT_VALUE: u8 = 0x34;
+        const EXPECTED_VALUE: u8 = LEFT_VALUE + RIGHT_VALUE;
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         
-        add(&mut registers, VALUE_TO_ADD);
+        let result = add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
-        assert_eq!(as_hex!(registers.a), as_hex!(EXPECTED_VALUE));
+        assert_eq!(as_hex!(result), as_hex!(EXPECTED_VALUE));
     }
     
     #[test]
     fn test_add_becomes_zero_sets_zero_flag_to_true() {
-        const INITIAL_A: u8 = 0x12;
-        const VALUE_TO_ADD: u8 = (0x00 as u8).overflowing_sub(INITIAL_A).0;
-        
+        const LEFT_VALUE: u8 = 0x12;
+        const RIGHT_VALUE: u8 = (0x00 as u8).overflowing_sub(LEFT_VALUE).0;
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.zero_flag.get(), true);
     }
     
     #[test]
     fn test_add_becomes_non_zero_sets_zero_flag_to_false() {
-        const INITIAL_A: u8 = 0x12;
-        const VALUE_TO_ADD: u8 = 0xFF - INITIAL_A;
-        
+        const LEFT_VALUE: u8 = 0x12;
+        const RIGHT_VALUE: u8 = 0xFF - LEFT_VALUE;
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.zero_flag.get(), false);
     }
     
     #[test]
     fn test_add_turns_subtract_flag_off() {
-        const INITIAL_A: u8 = 0x12;
-        const VALUE_TO_ADD: u8 = 0x34;
+        const LEFT_VALUE: u8 = 0x12;
+        const RIGHT_VALUE: u8 = 0x34;
         
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         registers.subtract_flag.set(true);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.subtract_flag.get(), false);
     }
     
     #[test]
     fn test_add_no_overflow_sets_carry_flag_off() {
-        const INITIAL_A: u8 = 0xFF;
-        const VALUE_TO_ADD: u8 = 0x00;
+        const LEFT_VALUE: u8 = 0xFF;
+        const RIGHT_VALUE: u8 = 0x00;
         
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         registers.carry_flag.set(true);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.carry_flag.get(), false);
     }
     
     #[test]
     fn test_add_overflowed_sets_carry_flag_on() {
-        const INITIAL_A: u8 = 0xFF;
-        const VALUE_TO_ADD: u8 = 0x1;
-        
+        const LEFT_VALUE: u8 = 0xFF;
+        const RIGHT_VALUE: u8 = 0x1;
+
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         registers.carry_flag.set(false);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.carry_flag.get(), true);
     }
     
     #[test]
     fn test_add_no_lower_nibble_overflow_sets_half_carry_flag_off() {
-        const INITIAL_A: u8 = 0xB0;
-        const VALUE_TO_ADD: u8 = 0x10;
+        const LEFT_VALUE: u8 = 0xB0;
+        const RIGHT_VALUE: u8 = 0x10;
         
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         registers.half_carry_flag.set(true);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.half_carry_flag.get(), false);
     }
     
     #[test]
     fn test_add_lower_nibble_overflowed_sets_half_carry_flag_on() {
-        const INITIAL_A: u8 = 0xFF;
-        const VALUE_TO_ADD: u8 = 0x1;
+        const LEFT_VALUE: u8 = 0xFF;
+        const RIGHT_VALUE: u8 = 0x1;
         
         let mut registers = Registers::new();
-		registers.a.set(INITIAL_A);
         registers.half_carry_flag.set(false);
         
-        add(&mut registers, VALUE_TO_ADD);
+        add(&mut registers, LEFT_VALUE, RIGHT_VALUE);
         
         assert_eq!(registers.half_carry_flag.get(), true);
     }
--- src/instructions/add/instructions.rs
+++ src/instructions/add/instructions.rs
@@ -1,16 +1,23 @@
 use crate::instructions::instruction::{Instruction};
 use crate::instructions::add::add_from_register::{AddFromRegister};
+use crate::instructions::add::add::{add};
+use crate::instructions::sources::register_source::RegisterSource;
 use crate::registers::register_names::{RegisterName};
+use crate::instructions::common::binary_byte_op::BinaryByteOp;
+
+fn build_add_instruction(other_source_name: RegisterName) -> Option<Box<dyn Instruction>> {
+    return Some(Box::new(BinaryByteOp::new_inplace_a_op(Box::new(RegisterSource::new(other_source_name)), add)));
+}
 
 pub fn load_instruction(instruction_byte: u8) -> Option<Box<dyn Instruction>> {
     return match instruction_byte {
-        0x80 => Some(Box::new(AddFromRegister::new(RegisterName::B))),
-        0x81 => Some(Box::new(AddFromRegister::new(RegisterName::C))),
-        0x82 => Some(Box::new(AddFromRegister::new(RegisterName::D))),
-        0x83 => Some(Box::new(AddFromRegister::new(RegisterName::E))),
-        0x84 => Some(Box::new(AddFromRegister::new(RegisterName::H))),
-        0x85 => Some(Box::new(AddFromRegister::new(RegisterName::L))),
-        0x87 => Some(Box::new(AddFromRegister::new(RegisterName::A))),
+        0x80 => build_add_instruction(RegisterName::B),
+        0x81 => build_add_instruction(RegisterName::C),
+        0x82 => build_add_instruction(RegisterName::D),
+        0x83 => build_add_instruction(RegisterName::E),
+        0x84 => build_add_instruction(RegisterName::H),
+        0x85 => build_add_instruction(RegisterName::L),
+        0x87 => build_add_instruction(RegisterName::A),
         _ => None,
     };
 }
