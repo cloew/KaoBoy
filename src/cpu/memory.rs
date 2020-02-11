a7a15fd527f0273e0f9258b147bdf3c62acd7809
Chris Loew <cloew123@gmail.com>
2020-02-10 19:21:50 -0800

    Added ability to bootstrap the Gameboy memory

--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -8,6 +8,12 @@ impl Memory {
         return Memory {_memory: [0; 0xFFFF]};
     }
     
+    pub fn bootstrap(&mut self, bootstrap_data: &[u8]) {
+        for (place, data) in self._memory.iter_mut().zip(bootstrap_data.iter()) {
+            *place = *data
+        }
+    }
+    
     pub fn read_byte(&self, address: u16) -> u8 {
         return self._memory[address as usize];
     }
@@ -21,6 +27,21 @@ impl Memory {
 mod tests {
     use super::*;
     use crate::{as_hex};
+    
+    #[test]
+    fn test_bootstrap_sets_bytes() {
+        const BOOTSTRAP_BYTES: [u8; 10] = [
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x16, 0x17, 0x18, 0x1A, 0x1B,
+        ];
+        let mut memory = Memory::new();
+        
+        memory.bootstrap(&BOOTSTRAP_BYTES);
+        
+        for (i, expected) in BOOTSTRAP_BYTES.iter().enumerate() {
+            let address: u16 = i as u16;
+            assert_eq!(as_hex!(memory.read_byte(address)), as_hex!(expected));
+        }
+    }
     
     #[test]
     fn test_read_byte_returns_byte() {
--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,54 @@ use std::collections::HashMap;
 
 fn main() {
     let mut memory = Memory::new();
-    // Add
-    memory.set_byte(0, 0x80); // 0x00+0x01 = 0x01
-    memory.set_byte(1, 0x81); // 0x01+0x02 = 0x03
-    memory.set_byte(2, 0x82); // 0x03+0x04 = 0x07
-    memory.set_byte(3, 0x83); // 0x07+0x08 = 0x0F
-    memory.set_byte(4, 0x84); // 0x0F+0x10 = 0x1F
-    memory.set_byte(5, 0x85); // 0x1F+0x20 = 0x3F
-    memory.set_byte(6, 0x87); // 0x3F+0x3F = 0x7E
-    memory.set_byte(7, 0xC6); // 0x7E+0x01 = 0x7F
-    memory.set_byte(8, 0x01);
-    // Subtract
-    memory.set_byte(9, 0x90);
-    memory.set_byte(10, 0x91);
-    memory.set_byte(11, 0x92);
-    memory.set_byte(12, 0x93);
-    memory.set_byte(13, 0x94);
-    memory.set_byte(14, 0x95);
-    memory.set_byte(15, 0x97);
-    memory.set_byte(16, 0xD6);
-    memory.set_byte(17, 0x01);
-    // XOR
-    memory.set_byte(18, 0xA8);
-    memory.set_byte(19, 0xA9);
-    memory.set_byte(20, 0xAA);
-    memory.set_byte(21, 0xAB);
-    memory.set_byte(22, 0xAC);
-    memory.set_byte(23, 0xAD);
-    memory.set_byte(24, 0xAF);
-    memory.set_byte(25, 0xEE);
-    memory.set_byte(26, 0xF0);
-    memory.set_byte(27, 0xEE);
-    memory.set_byte(28, 0x0F);
-    // LOAD
-    memory.set_byte(29, 0x78);
-    memory.set_byte(30, 0x79);
-    memory.set_byte(31, 0x7A);
-    memory.set_byte(32, 0x7B);
-    memory.set_byte(33, 0x7C);
-    memory.set_byte(34, 0x7D);
-    memory.set_byte(35, 0x7F);
-    memory.set_byte(36, 0x3E);
-    memory.set_byte(37, 0xFF);
     
+    let program: [u8; 38] = [
+        // Add
+        0x80, // 0x00+0x01 = 0x01
+        0x81, // 0x01+0x02 = 0x03
+        0x82, // 0x03+0x04 = 0x07
+        0x83, // 0x07+0x08 = 0x0F
+        0x84, // 0x0F+0x10 = 0x1F
+        0x85, // 0x1F+0x20 = 0x3F
+        0x87, // 0x3F+0x3F = 0x7E
+        0xC6, // 0x7E+0x01 = 0x7F
+        0x01,
+        // Subtract
+        0x90,
+        0x91,
+        0x92,
+        0x93,
+        0x94,
+        0x95,
+        0x97,
+        0xD6,
+        0x01,
+        // XOR
+        0xA8,
+        0xA9,
+        0xAA,
+        0xAB,
+        0xAC,
+        0xAD,
+        0xAF,
+        0xEE,
+        0xF0,
+        0xEE,
+        0x0F,
+        // LOAD
+        0x78,
+        0x79,
+        0x7A,
+        0x7B,
+        0x7C,
+        0x7D,
+        0x7F,
+        0x3E,
+        0xFF,
+    ];
+    
+    
+    memory.bootstrap(&program);
 	let mut cpu = Cpu::new(memory);
     cpu._registers.borrow_mut().b.set(1);
     cpu._registers.borrow_mut().c.set(2);
