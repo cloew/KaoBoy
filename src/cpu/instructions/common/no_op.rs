90415bb0d0bfcd705d55ad6b46c3c72c26e8044d
Chris Loew <cloew123@gmail.com>
2020-02-17 20:06:41 -0800

    Added a no op for shorts

--- src/cpu/instructions/common/no_op.rs
+++ src/cpu/instructions/common/no_op.rs
@@ -3,6 +3,9 @@ use super::super::super::instruction_context::InstructionContext;
 pub fn byte_no_op(_context: &mut InstructionContext, value: u8) -> u8 {
     return value;
 }
+pub fn short_no_op(_context: &mut InstructionContext, value: u16) -> u16 {
+    return value;
+}
 
 #[cfg(test)]
 mod tests {
@@ -11,11 +14,21 @@ mod tests {
     use crate::cpu::testing::build_test_instruction_context;
     
     #[test]
-    fn test_run_calls_source_op_and_destination() {
+    fn test_byte_no_op_calls_source_op_and_destination() {
         const VALUE: u8 = 0x12;
         let mut context = build_test_instruction_context();
         
         let result = byte_no_op(&mut context, VALUE);
+        
+        assert_eq!(as_hex!(result), as_hex!(VALUE));
+    }
+    
+    #[test]
+    fn test_short_no_op_calls_source_op_and_destination() {
+        const VALUE: u16 = 0x1234;
+        let mut context = build_test_instruction_context();
+        
+        let result = short_no_op(&mut context, VALUE);
         
         assert_eq!(as_hex!(result), as_hex!(VALUE));
     }
