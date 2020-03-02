7d92ba54ddeb340959876b5631255b0ed178e1ad
Chris Loew <cloew123@gmail.com>
2020-03-01 17:14:56 -0800

    Added op to inc double registers

--- src/cpu/instructions/inc/inc.rs
+++ src/cpu/instructions/inc/inc.rs
@@ -11,6 +11,11 @@ pub fn inc(context: &mut InstructionContext, value: u8) -> u8 {
     return new_value;
 }
 
+pub fn inc_double_register(context: &mut InstructionContext, value: u16) -> u16 {
+    let (new_value, overflow) = value.overflowing_add(1);
+    return new_value;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,6 +93,61 @@ mod tests {
         context.registers_mut().half_carry_flag.set(false);
         
         inc(&mut context, VALUE);
+        
+        assert_eq!(context.registers().half_carry_flag.get(), true);
+    }
+    
+    #[test]
+    fn test_inc_double_register_returns_incremented_value() {
+        const VALUE: u16 = 0x1234;
+        const EXPECTED_VALUE: u16 = VALUE + 1;
+        let mut context = build_test_instruction_context();
+        
+        let result = inc_double_register(&mut context, VALUE);
+        
+        assert_eq!(as_hex!(result), as_hex!(EXPECTED_VALUE));
+    }
+    
+    #[test]
+    fn test_inc_double_register_doesnt_change_zero_flag() {
+        const VALUE: u16 = 0xFF;
+        let mut context = build_test_instruction_context();
+        context.registers_mut().zero_flag.set(true);
+        
+        inc_double_register(&mut context, VALUE);
+        
+        assert_eq!(context.registers().zero_flag.get(), true);
+    }
+    
+    #[test]
+    fn test_inc_double_register_doesnt_change_subtract_flag() {
+        const VALUE: u16 = 0xFF;
+        let mut context = build_test_instruction_context();
+        context.registers_mut().subtract_flag.set(true);
+        
+        inc_double_register(&mut context, VALUE);
+        
+        assert_eq!(context.registers().subtract_flag.get(), true);
+    }
+    
+    #[test]
+    fn test_inc_double_register_doesnt_change_carry_flag() {
+        const VALUE: u16 = 0xFF;
+        let mut context = build_test_instruction_context();
+        context.registers_mut().carry_flag.set(true);
+        
+        inc_double_register(&mut context, VALUE);
+        
+        assert_eq!(context.registers().carry_flag.get(), true);
+    }
+    
+    #[test]
+    fn test_inc_double_register_doesnt_change_half_carry_flag() {
+        const VALUE: u16 = 0xFF;
+        let mut context = build_test_instruction_context();
+        context.registers_mut().half_carry_flag.set(true);
+        
+        inc_double_register(&mut context, VALUE);
         
         assert_eq!(context.registers().half_carry_flag.get(), true);
     }
--- src/cpu/instructions/inc/mod.rs
+++ src/cpu/instructions/inc/mod.rs
@@ -3,3 +3,4 @@ pub mod instructions;
 mod inc;
 
 pub use inc::inc;
+pub use inc::inc_double_register;
