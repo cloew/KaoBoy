3ecce5ac100a987ea301e054ffd93becbb58f561
Chris Loew <cloew123@gmail.com>
2020-03-09 20:57:59 -0700

    Added basic handling of Prefix instructions

--- src/cpu/instructions/instructions.rs
+++ src/cpu/instructions/instructions.rs
@@ -6,10 +6,27 @@ use super::xor;
 use super::instruction::Instruction;
 use super::super::ProgramCounter;
 use crate::as_hex;
+use std::boxed::Box;
+use std::option::Option;
+
+type PackageInstructionLoader = fn(u8) -> Option<Box<(dyn Instruction + 'static)>>;
+
+static PREFIX_INSTRUCTION: u8 = 0xCB;
 
 pub fn load_instruction(program: &mut ProgramCounter) -> Box<dyn Instruction> {
-    let package_instruction_loaders: Vec<fn(u8) -> 
-            std::option::Option<std::boxed::Box<(dyn Instruction + 'static)>>>
+    
+    let instruction_byte = program.read_next_byte();
+    
+    if instruction_byte == PREFIX_INSTRUCTION {
+        let instruction_byte = program.read_next_byte();
+        return load_prefix_instruction(instruction_byte);
+    } else {
+        return load_standard_instruction(instruction_byte);
+    }
+}
+
+fn load_standard_instruction(instruction_byte: u8) -> Box<dyn Instruction> {
+    let package_instruction_loaders: Vec<PackageInstructionLoader>
             = vec![
         add::instructions::load_instruction,
         inc::instructions::load_instruction,
@@ -18,11 +35,7 @@ pub fn load_instruction(program: &mut ProgramCounter) -> Box<dyn Instruction> {
         xor::instructions::load_instruction,
     ];
     
-    let instruction_byte = program.read_next_byte();
-    
-    let next_instruction = package_instruction_loaders.iter().find_map(
-        |load_from_package| load_from_package(instruction_byte)
-    );
+    let next_instruction = load_instruction_from_packages(instruction_byte, package_instruction_loaders);
     
     return match next_instruction {
         Some(instruction) => {
@@ -30,6 +43,27 @@ pub fn load_instruction(program: &mut ProgramCounter) -> Box<dyn Instruction> {
         },
         None => panic!("Unknown instruction: {}", as_hex!(instruction_byte)),
     }
+}
+
+fn load_prefix_instruction(instruction_byte: u8) -> Box<dyn Instruction> {
+    let package_instruction_loaders: Vec<PackageInstructionLoader> = vec![];
+    
+    let next_instruction = load_instruction_from_packages(instruction_byte, package_instruction_loaders);
+    
+    return match next_instruction {
+        Some(instruction) => {
+            instruction
+        },
+        None => panic!("Unknown prefix instruction: {}", as_hex!(instruction_byte)),
+    }
+}
+
+fn load_instruction_from_packages(instruction_byte: u8, package_instruction_loaders: Vec<PackageInstructionLoader>) -> Option<Box<dyn Instruction>> {
+    let next_instruction = package_instruction_loaders.iter().find_map(
+        |load_from_package| load_from_package(instruction_byte)
+    );
+    
+    return next_instruction;
 }
 
 #[cfg(test)]
@@ -47,4 +81,17 @@ mod tests {
         
         // Not sure if there'e anything I can assert on
     }
+    
+    // TODO: Uncomment once Prefix has its first actual instruction
+    /*#[test]
+    fn test_load_instruction_prefix_instruction_returns_instruction() {
+        const BIT_INSTRUCTION: u8 = 0x40;
+        let mut program = build_test_program_counter();
+        program._memory.borrow_mut().write_byte(0x0000, PREFIX_INSTRUCTION);
+        program._memory.borrow_mut().write_byte(0x0001, BIT_INSTRUCTION);
+        
+        load_instruction(&mut program);
+        
+        // Not sure if there'e anything I can assert on
+    }*/
 }
