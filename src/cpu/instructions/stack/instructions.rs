ba8bba667f79b2e62df5c41eeed6c32a4c5ee9f6
Chris Loew <cloew123@gmail.com>
2020-04-14 19:56:47 -0700

    Added push instructions

--- src/cpu/instructions/stack/instructions.rs
+++ src/cpu/instructions/stack/instructions.rs
@@ -1,13 +1,23 @@
 use super::super::jump::{JumpConditionFn, always, is_carry_flag_off, is_carry_flag_on, is_zero_flag_off, is_zero_flag_on};
 use super::call_instruction::CallInstruction;
+use super::push_instruction::PushInstruction;
 use super::super::instruction::Instruction;
 use super::super::sources::{ByteSource, ConstantByteSource};
+use super::super::super::registers::DoubleRegisterName;
 use crate::{boxed, optional_boxed};
 
 fn build_call_instruction(condition: JumpConditionFn) -> Option<Box<dyn Instruction>> {
     return optional_boxed!(
         CallInstruction::new(
             condition
+        )
+    );
+}
+
+fn build_push_instruction(register: DoubleRegisterName) -> Option<Box<dyn Instruction>> {
+    return optional_boxed!(
+        PushInstruction::new_for_register(
+            register
         )
     );
 }
@@ -20,6 +30,11 @@ pub fn load_instruction(instruction_byte: u8) -> Option<Box<dyn Instruction>> {
         0xCC => build_call_instruction(is_zero_flag_on),
         0xD4 => build_call_instruction(is_carry_flag_off),
         0xDC => build_call_instruction(is_carry_flag_on),
+        // Push Instructions
+        0xC5 => build_push_instruction(DoubleRegisterName::BC),
+        0xD5 => build_push_instruction(DoubleRegisterName::DE),
+        0xE5 => build_push_instruction(DoubleRegisterName::HL),
+        0xF5 => build_push_instruction(DoubleRegisterName::AF),
         _ => None,
     };
 }
