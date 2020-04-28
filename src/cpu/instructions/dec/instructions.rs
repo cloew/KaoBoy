6d61e8bf840d5f571637e0a955151d2c383a09d7
Chris Loew <cloew123@gmail.com>
2020-04-27 18:28:12 -0700

    Renamed the helper fucntions to be more clear about their purpose

--- src/cpu/instructions/dec/instructions.rs
+++ src/cpu/instructions/dec/instructions.rs
@@ -6,7 +6,7 @@ use super::super::destinations::{DoubleRegisterDestination, RegisterDestination}
 use super::super::super::registers::{DoubleRegisterName, RegisterName};
 use crate::{boxed, optional_boxed};
 
-fn build_dec_instruction(register: RegisterName) -> Option<Box<dyn Instruction>> {
+fn build_dec_register(register: RegisterName) -> Option<Box<dyn Instruction>> {
     return optional_boxed!(
         UnaryByteOp::new(
             boxed!(RegisterSource::new(register)),
@@ -16,7 +16,7 @@ fn build_dec_instruction(register: RegisterName) -> Option<Box<dyn Instruction>>
     );
 }
 
-fn build_dec_instruction_double_register(register: DoubleRegisterName) -> Option<Box<dyn Instruction>> {
+fn build_dec_double_register(register: DoubleRegisterName) -> Option<Box<dyn Instruction>> {
     return optional_boxed!(
         UnaryShortOp::new(
             boxed!(DoubleRegisterSource::new(register)),
@@ -29,17 +29,17 @@ fn build_dec_instruction_double_register(register: DoubleRegisterName) -> Option
 pub fn load_instruction(instruction_byte: u8) -> Option<Box<dyn Instruction>> {
     return match instruction_byte {
         // Decrement Registers
-        0x05 => build_dec_instruction(RegisterName::B),
-        0x15 => build_dec_instruction(RegisterName::D),
-        0x25 => build_dec_instruction(RegisterName::H),
-        0x0D => build_dec_instruction(RegisterName::C),
-        0x1D => build_dec_instruction(RegisterName::E),
-        0x2D => build_dec_instruction(RegisterName::L),
-        0x3D => build_dec_instruction(RegisterName::A),
+        0x05 => build_dec_register(RegisterName::B),
+        0x15 => build_dec_register(RegisterName::D),
+        0x25 => build_dec_register(RegisterName::H),
+        0x0D => build_dec_register(RegisterName::C),
+        0x1D => build_dec_register(RegisterName::E),
+        0x2D => build_dec_register(RegisterName::L),
+        0x3D => build_dec_register(RegisterName::A),
         // Decrement Double Registers
-        0x08 => build_dec_instruction_double_register(DoubleRegisterName::BC),
-        0x18 => build_dec_instruction_double_register(DoubleRegisterName::DE),
-        0x28 => build_dec_instruction_double_register(DoubleRegisterName::HL),
+        0x08 => build_dec_double_register(DoubleRegisterName::BC),
+        0x18 => build_dec_double_register(DoubleRegisterName::DE),
+        0x28 => build_dec_double_register(DoubleRegisterName::HL),
         _ => None,
     };
 }
@@ -50,9 +50,9 @@ mod tests {
     
     #[test]
     fn test_load_instruction_returns_instruction() {
-        const LOAD_INSTRUCTION: u8 = 0x40;
+        const DEC_INSTRUCTION: u8 = 0x05;
         
-        load_instruction(LOAD_INSTRUCTION);
+        load_instruction(DEC_INSTRUCTION);
         
         // Not sure if there'e anything I can assert on
     }
