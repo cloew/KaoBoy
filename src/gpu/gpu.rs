55de2360d04580cdae9f67bf30e5be8045de2b34
Chris Loew <cloew123@gmail.com>
2020-07-13 18:56:32 -0700

    Added tests for the tick method of the GPU

--- src/gpu/gpu.rs
+++ src/gpu/gpu.rs
@@ -5,8 +5,8 @@ use std::fs;
 use std::rc::Rc;
 use std::cell::RefCell;
 
-const LAST_SCAN_LINE: u16 = 144;
-const LAST_VBLANK_LINE: u16 = 154;
+const LAST_SCAN_LINE: u16 = 143;
+const LAST_VBLANK_LINE: u16 = 153;
 
 const HBLANK_MODE: u16 = 0;
 const VBLANK_MODE: u16 = 1;
@@ -46,7 +46,7 @@ impl Gpu {
             })),
             hBlankMode: rc_refcell!(GpuMode::new(HBLANK_MODE, HBLANK_CYCLES, |gpu: &mut Gpu| -> u16 {
                 gpu.goToNextLine();
-                if (gpu.currentLine == LAST_SCAN_LINE) {
+                if (gpu.currentLine == LAST_SCAN_LINE + 1) {
                     // Render screen
                     return VBLANK_MODE;
                 } else {
@@ -55,7 +55,7 @@ impl Gpu {
             })),
             vBlankMode: rc_refcell!(GpuMode::new(VBLANK_MODE, VBLANK_CYCLES, |gpu: &mut Gpu| -> u16 {
                 gpu.goToNextLine();
-                if (gpu.currentLine == LAST_VBLANK_LINE) {
+                if (gpu.currentLine == LAST_VBLANK_LINE + 1) {
                     return OAM_MODE;
                 } else {
                     return VBLANK_MODE;
@@ -75,7 +75,7 @@ impl Gpu {
         if (self.cyclesLeft == 0) {
             let nextMode = self.exitMode();
             self.enterMode(nextMode);
-            self.tick(cycles);
+            self.tick(cyclesLeftToSpin);
         }
     }
     
@@ -106,8 +106,9 @@ impl Gpu {
     
     fn spin(&mut self, cycles: u16) -> u16 {
         if (cycles > self.cyclesLeft) {
+            let remainingCycles = cycles - self.cyclesLeft;
             self.cyclesLeft = 0;
-            return cycles - self.cyclesLeft;
+            return remainingCycles;
         } else {
             self.cyclesLeft -= cycles;
             return 0;
@@ -130,9 +131,127 @@ mod tests {
     }
     
     #[test]
-    fn test_tick() {
+    fn test_tick_decreasesCycleCount() {
+        const EXPECTED_REMAINING_CYCLES: u16 = 5;
         let mut gpu = Gpu::new();
+        gpu.initialize();
         
-        //emulator.bootstrap();
+        gpu.tick(OAM_CYCLES-EXPECTED_REMAINING_CYCLES);
+        
+        assert_eq!(gpu.mode, OAM_MODE);
+        assert_eq!(gpu.cyclesLeft, EXPECTED_REMAINING_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_overSpins_decreasesCycleCountFromNextMode() {
+        const EXPECTED_REMAINING_CYCLES: u16 = 5;
+        const CYCLES_TO_SPIN: u16 = OAM_CYCLES + VRAM_CYCLES - EXPECTED_REMAINING_CYCLES;
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        
+        gpu.tick(CYCLES_TO_SPIN);
+        
+        assert_eq!(gpu.mode, VRAM_MODE);
+        assert_eq!(gpu.cyclesLeft, EXPECTED_REMAINING_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesOamMode_progressesToVramMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(OAM_MODE);
+        
+        gpu.tick(OAM_CYCLES);
+        
+        assert_eq!(gpu.mode, VRAM_MODE);
+        assert_eq!(gpu.cyclesLeft, VRAM_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesVramMode_progressesToHBlankMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(VRAM_MODE);
+        
+        gpu.tick(VRAM_CYCLES);
+        
+        assert_eq!(gpu.mode, HBLANK_MODE);
+        assert_eq!(gpu.cyclesLeft, HBLANK_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesHBlankMode_notAtLastScanLine_progressesToOamMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(HBLANK_MODE);
+        gpu.currentLine = LAST_SCAN_LINE-1;
+        
+        gpu.tick(HBLANK_CYCLES);
+        
+        assert_eq!(gpu.mode, OAM_MODE);
+        assert_eq!(gpu.cyclesLeft, OAM_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesHBlankMode_atLastScanLine_progressesToVBlankMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(HBLANK_MODE);
+        gpu.currentLine = LAST_SCAN_LINE;
+        
+        gpu.tick(HBLANK_CYCLES);
+        
+        assert_eq!(gpu.mode, VBLANK_MODE);
+        assert_eq!(gpu.cyclesLeft, VBLANK_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesHBlankMode_advancesToNextLine() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(HBLANK_MODE);
+        let nextLine = gpu.currentLine + 1;
+        
+        gpu.tick(HBLANK_CYCLES);
+        
+        assert_eq!(gpu.currentLine, nextLine);
+    }
+    
+    #[test]
+    fn test_tick_finishesVBlankMode_notAtLastVBlankLine_progressesToVBlankMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(VBLANK_MODE);
+        gpu.currentLine = LAST_VBLANK_LINE-1;
+        
+        gpu.tick(VBLANK_CYCLES);
+        
+        assert_eq!(gpu.mode, VBLANK_MODE);
+        assert_eq!(gpu.cyclesLeft, VBLANK_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesVBlankMode_atLastVBlankLine_progressesToOamMode() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(VBLANK_MODE);
+        gpu.currentLine = LAST_VBLANK_LINE;
+        
+        gpu.tick(VBLANK_CYCLES);
+        
+        assert_eq!(gpu.mode, OAM_MODE);
+        assert_eq!(gpu.cyclesLeft, OAM_CYCLES);
+    }
+    
+    #[test]
+    fn test_tick_finishesVBlankMode_advancesToNextLine() {
+        let mut gpu = Gpu::new();
+        gpu.initialize();
+        gpu.enterMode(VBLANK_MODE);
+        let nextLine = gpu.currentLine + 1;
+        
+        gpu.tick(VBLANK_CYCLES);
+        
+        assert_eq!(gpu.currentLine, nextLine);
     }
 }
